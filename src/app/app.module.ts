49ff9fea626ee09ec5ae679983dd3112fe6f1dca
Mike Huang <wellwind@mail2000.com.tw>
2021-12-02 11:22:11 +0800

    將畫面切成元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    HeaderComponent,
+    ContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
