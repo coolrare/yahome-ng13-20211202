4a46b4df5268c6ff6f15ba866a979d8e5636d860
Mike Huang <wellwind@mail2000.com.tw>
2021-12-02 11:32:44 +0800

    練習內嵌繫結 & 屬性繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  title = 'The Will Will Web';
+  subTitle = '記載著 Will 在網路世界的學習心得與技術分享';
+  link = 'https://google.com/';
+  fbTitle = 'Will 保哥的技術交流中心';
 
   constructor() { }
 
